13cb8943457f17efae2829a3874fe708e6b91659
SrJuggernaut <jugger@srjuggernaut.dev>
2023-08-09 12:11:07 -0600

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Typography from '@/components/ui/Typography'
+import { type FC } from 'react'
+
+const Home: FC = () => {
+  return (
+    <>
+      <Typography variant="h1" align="center" >EntGamers Next Template</Typography>
+      <Typography variant="body1">A template for EntGamers websites</Typography>
+    </>
+  )
+}
+
+export default Home

bf8e1d071cb97fa4a2800a2463e2d010a2db2bb8
SrJuggernaut <jugger@srjuggernaut.dev>
2023-08-11 15:45:35 -0600

    fix: missing port on workflows

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
     args: 'start',
     env: {
       NODE_ENV: 'production',
-      PORT: process.env.PORT || 3700,
+      PORT: process.env.PORT || 3702,
       NEXT_PUBLIC_APPWRITE_PROJECT_ID: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID|| '',
       NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || '',
       APPWRITE_API_KEY: process.env.APPWRITE_API_KEY || '',
@@ -23,6 +23,7 @@ module.exports = {
       'pre-deploy': 'yarn install && yarn run build',
       'post-deploy': 'pm2 startOrRestart ecosystem.config.js',
       env: {
+        PORT: process.env.PORT || 3700,
         NEXT_PUBLIC_APPWRITE_PROJECT_ID: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
         NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
         APPWRITE_API_KEY: process.env.APPWRITE_API_KEY,
@@ -39,11 +40,13 @@ module.exports = {
       'pre-deploy': 'yarn install && yarn run build',
       'post-deploy': 'pm2 startOrRestart ecosystem.config.js',
       env: {
+        PORT: process.env.PORT || 3701,
         NEXT_PUBLIC_APPWRITE_PROJECT_ID: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
         NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
         APPWRITE_API_KEY: process.env.APPWRITE_API_KEY,
         NEXT_PUBLIC_RECAPTCHA_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_KEY,
         RECAPTCHA_SECRET: process.env.RECAPTCHA_SECRET
       }
+    }
   },
 }

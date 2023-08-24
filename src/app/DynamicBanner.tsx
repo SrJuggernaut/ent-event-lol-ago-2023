de7414585a4bb517f6d68ab481c0ca71d16d1a57
SrJuggernaut <jugger@srjuggernaut.dev>
2023-08-24 00:04:10 -0600

    feat: event end

--- src/app/DynamicBanner.tsx
+++ src/app/DynamicBanner.tsx
@@ -9,40 +9,7 @@ import { type FC } from 'react'
 
 const DynamicBanner: FC = () => {
   const { user, teams } = useAppSelector(state => state.session)
-  if (user === undefined) {
-    return (
-      <div
-        className={css({
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginTop: 'medium',
-          marginBottom: 'medium',
-          minHeight: '30vh'
-        })}
-      >
-        <Typography variant="h2" align="center" component="div">
-          Inscripciones abiertas
-        </Typography>
-        <Typography variant="body1" align="center">
-          El evento se llevará a cabo del <Typography component="strong" color="info" weight="bold">16 de Agosto al 23 de Agosto</Typography>.
-        </Typography>
-        <NextLink
-          className={
-            button({
-              variant: 'solid',
-              color: 'info',
-              size: 'medium'
-            })
-          }
-          href="/register"
-        >
-          Click para registrarte
-        </NextLink>
-      </div>
-    )
-  }
+
   if (user !== undefined && teams !== undefined && teams.teams.some((team) => team.$id === REVIEWER_TEAM_ID || team.$id === ADMIN_TEAM_ID)) {
     return (
       <div
@@ -76,8 +43,7 @@ const DynamicBanner: FC = () => {
         </NextLink>
       </div>
     )
-  }
-  if (user !== undefined && user.prefs.summonerName === undefined) {
+  } else {
     return (
       <div
         className={css({
@@ -91,10 +57,10 @@ const DynamicBanner: FC = () => {
         })}
       >
         <Typography variant="h2" align="center" component="div">
-          Registra tu nombre de invocador
+          Gracias por participar
         </Typography>
         <Typography variant="body1" align="center">
-          Para poder participar en el evento, necesitas registrar tu nombre de invocador, recuerda que después de registrar tu nombre de invocador no podrás cambiarlo.
+          El evento ha terminado, gracias por participar. La entrega de premios se realizará durante las próximas semanas. Puedes revisar los resultados en el siguiente enlace.
         </Typography>
         <NextLink
           className={
@@ -104,43 +70,9 @@ const DynamicBanner: FC = () => {
               size: 'medium'
             })
           }
-          href="/profile"
+          href="/puntuacion"
         >
-          Registrar nombre de invocador
-        </NextLink>
-      </div>
-    )
-  }
-  if (user?.prefs.summonerName !== undefined) {
-    return (
-      <div
-        className={css({
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginTop: 'medium',
-          marginBottom: 'medium',
-          minHeight: '30vh'
-        })}
-      >
-        <Typography variant="h2" align="center" component="div">
-          Envía tu evidencia
-        </Typography>
-        <Typography variant="body1" align="center">
-          Envía evidencia de tus partidas para comenzar a acumular puntos.
-        </Typography>
-        <NextLink
-          className={
-            button({
-              variant: 'solid',
-              color: 'info',
-              size: 'medium'
-            })
-          }
-          href="/evidence-upload"
-        >
-          Enviar evidencia
+          Revisar resultados
         </NextLink>
       </div>
     )
